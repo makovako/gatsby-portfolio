36b5a324a4174fa6cc917802a42934bfbe270504
Marek Valko <valko.marek@gmail.com>
2020-02-13 11:37:09 +0100

    added nav and footer for main container

--- src/components/container.js
+++ src/components/container.js
@@ -1,5 +1,11 @@
-import React from 'react'
-import containerStyle from './container.module.css'
-export default ({children}) => (
-    <div className={containerStyle.container}>{children}</div>
+import React from "react"
+import styles from "./container.module.css"
+import Navigation from '../components/navigation'
+import Footer from '../components/footer'
+export default ({ children }) => (
+  <div className={styles.container}>
+    <Navigation></Navigation>
+    {children}
+    <Footer></Footer>
+  </div>
 )
